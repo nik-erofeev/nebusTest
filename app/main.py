c28230d14d65c9120a24ba10bf7dc08719adeab2
Nik <erofeev.nik.it@yandex.ru>
2025-01-21 18:32:03 +0300

    add alembic

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
